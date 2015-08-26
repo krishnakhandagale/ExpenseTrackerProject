4013905c7d13e9094876a85aefb98888ece00602
krishnakhandagale <khandagalekrishna8@gmail.com>
2015-08-26 23:39:54 +0530

    Sliding Menu Activity Added:Krishna

---
+++ app/src/main/java/com/app/SlidingMenuActivity.java
@@ -0,0 +1,18 @@
+package com.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.krishna.expensetracker.R;
+
+/**
+ * Created by krishna on 8/26/2015.
+ */
+public class SlidingMenuActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sliding_menu_screen);
+    }
+}
